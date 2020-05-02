cfd97050f740116b58b61b52b9a94c187f0465d6
Jaguar <businessdeleu@gmail.com>
2020-05-02 21:22:34 +0300

    original_modified

--- scripts/original.js
+++ scripts/original.js
@@ -10,18 +10,50 @@ let appData = {
     savings: false
 };    
 
-for(let i = 0; i < 2; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+/* for(let i = 0; i < 2; i++) {
+    label: let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
         b = prompt("Во сколько обойдется?", '');
 
-    if ( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+    if (( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
          console.log("done");
          appData.expenses[a] = b;
     } else {
+        console.log("некорректные данные");
+        break label;
+    }
+}; */
+let i = 0;
+/* while(i < 2) {
+    i++;
+    label: let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+                b = prompt("Во сколько обойдется?", '');
 
+    if (( ( typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+        console.log("doen");
+        appData.expenses[a] = b;
+        else {
+            console.log("некорректные данные");
+            break label;
+        }
     }
 
-};
+} */
+
+do {
+    i++;
+    label: let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+                b = prompt("Во сколько обойдется?", '');
+
+    if ( ( typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+        console.log("doen");
+        appData.expenses[a] = b;
+        else {
+            console.log("некорректные данные");
+            break label;
+        }
+    }
+
+} while (i <2);
 
 appData.moneyPerDay = appData.budget / 30;
 alert("Ежедневный бюджет: " + appData.moneyPerDay);
